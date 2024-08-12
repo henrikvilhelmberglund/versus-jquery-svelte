a0c961525c79b88df3f656915e38c30f880b9e4f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-08-12 19:24:04 +0200

    feat: add +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<h1>{$page.status}: {$page.error.message}</h1>
+<img alt={$page.error.message} src={`https://http.cat/${$page.status}`} />
